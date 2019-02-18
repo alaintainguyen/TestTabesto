5cd6d74e3577ba90d02d27fe66f46e421381e746
Tai Nguyen <anguyen@viadeoteam.com>
2019-02-18 16:28:20 +0100

    listadapter patch

--- AndroidTai/app/src/main/java/com/tai/androidtai/modules/dashboard/DashboardListAdapter.kt
+++ AndroidTai/app/src/main/java/com/tai/androidtai/modules/dashboard/DashboardListAdapter.kt
@@ -10,7 +10,6 @@ import com.tai.androidtai.R
 import com.tai.androidtai.domain.bean.ResultBean
 
 import java.util.ArrayList
-import java.util.Random
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
@@ -27,10 +26,10 @@ class DashboardListAdapter internal constructor(private val mPresenter: Dashboar
 
     override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
         val resultBean = mItems[position] as ResultBean
-        holder.mMealName.text = resultBean.getName()
-        holder.mMealImage.setImageURI(resultBean.getImageUrl())
-        holder.mMealCategory.text = resultBean.getStatus()
-        holder.mMealPrice.text = resultBean.getGender()
+        holder.mName.text = resultBean.getName()
+        holder.mImage.setImageURI(resultBean.getImageUrl())
+        holder.mStatus.text = resultBean.getStatus()
+        holder.mGender.text = resultBean.getGender()
 //        holder.mLayout.setOnClickListener { v -> mPresenter.goToMealDetails(resultBean, rprice) }
     }
 
@@ -48,17 +47,17 @@ class DashboardListAdapter internal constructor(private val mPresenter: Dashboar
         @BindView(R.id.layout)
         lateinit var mLayout: ConstraintLayout
 
-        @BindView(R.id.dashboard_meal_name)
-        lateinit var mMealName: TextView
+        @BindView(R.id.dashboard_name)
+        lateinit var mName: TextView
 
-        @BindView(R.id.dashboard_meal_price)
-        lateinit var mMealPrice: TextView
+        @BindView(R.id.dashboard_gender)
+        lateinit var mGender: TextView
 
-        @BindView(R.id.dashboard_meal_category)
-        lateinit var mMealCategory: TextView
+        @BindView(R.id.dashboard_status)
+        lateinit var mStatus: TextView
 
-        @BindView(R.id.dashboard_meal_image)
-        lateinit var mMealImage: SimpleDraweeView
+        @BindView(R.id.dashboard_image)
+        lateinit var mImage: SimpleDraweeView
 
         init {
             ButterKnife.bind(this, itemView)
